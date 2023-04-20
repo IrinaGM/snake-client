7f863c9618f0cf392bc7a11dc6a8e8fe246f7da1
IrinaGM <irina.golionko@gmail.com>
2023-04-20 02:11:05 +0000

    implement setupInput and handleUserInput functions

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
-const {connect} = require("./client");
+const { connect } = require("./client");
+
+// setup interface to handle user input from stdin
+const setupInput = () => {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // event listener on input from user keyboard
+  stdin.on("data", (key) => handleUserInput(key));
+
+  return stdin;
+};
+
+// function that runs when input recieved from the user keyboard
+const handleUserInput = function (key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+  console.log(key);
+};
 
 connect();
+setupInput();
